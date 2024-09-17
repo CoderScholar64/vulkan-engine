77d849be6e28795ad9f9d0a65969b76791868da0
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-09-16 23:52:21 -0700

    More work done.

--- src/main.c
+++ src/main.c
@@ -1,26 +1,17 @@
 #include "SDL.h"
+#include <vulkan/vulkan.h>
+#include "SDL_vulkan.h"
 
 struct Context {
     char title[64];
     int x, y;
     int w, h;
     Uint32 flags;
-    SDL_Window *window_p;
+    VkInstance instance;
+    SDL_Window *pWindow;
 } context = {"Hello World", 0, 0, 1920, 1080, SDL_WINDOW_MAXIMIZED | SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN};
 
-int main(int argc, char **argv) {
-    if(SDL_Init( SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0 ) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init failed with %s", SDL_GetError());
-        return -1;
-    }
-
-    context.window_p = SDL_CreateWindow(context.title, context.x, context.y, context.w, context.h, context.flags);
-
-    if(context.window_p == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_CreateWindow failed with %s", SDL_GetError());
-        return -2;
-    }
-
+void loop() {
     SDL_Event event;
     int run = 1;
 
@@ -30,8 +21,99 @@ int main(int argc, char **argv) {
             run = 0;
         }
     }
+}
 
-    SDL_DestroyWindow(context.window_p);
+int initInstance() {
+    VkApplicationInfo applicationInfo;
+    memset(&applicationInfo, 0, sizeof(applicationInfo));
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    applicationInfo.pNext = NULL;
+    applicationInfo.pApplicationName = context.title;
+    applicationInfo.applicationVersion = 0;
+    applicationInfo.pEngineName = NULL;
+    applicationInfo.engineVersion = 0;
+    applicationInfo.apiVersion = VK_API_VERSION_1_0;
 
-    return 0;
+    unsigned int extensionCount = 0;
+    const char **ppExtensionNames = NULL;
+    if(!SDL_Vulkan_GetInstanceExtensions(context.pWindow, &extensionCount, ppExtensionNames)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Getting number of extensions had failed with %s", SDL_GetError());
+        return -3;
+    }
+    if(extensionCount != 0)
+        ppExtensionNames = malloc(sizeof(char*) * extensionCount);
+    if(!SDL_Vulkan_GetInstanceExtensions(context.pWindow, &extensionCount, ppExtensionNames)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Getting names of extensions had failed with %s", SDL_GetError());
+        free(ppExtensionNames);
+        return -4;
+    }
+
+    for(unsigned int i = 0; i < extensionCount; i++) {
+        SDL_Log( "[%i] %s", i, ppExtensionNames[i]);
+    }
+
+    VkInstanceCreateInfo instanceCreateInfo;
+    memset(&instanceCreateInfo, 0, sizeof(instanceCreateInfo));
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pNext = NULL;
+    instanceCreateInfo.pApplicationInfo = &applicationInfo;
+    instanceCreateInfo.enabledLayerCount = 0;
+    instanceCreateInfo.ppEnabledLayerNames = NULL;
+    instanceCreateInfo.enabledExtensionCount = extensionCount;
+    instanceCreateInfo.ppEnabledExtensionNames = ppExtensionNames;
+
+    VkResult result = vkCreateInstance(&instanceCreateInfo, NULL, &context.instance);
+
+    if(ppExtensionNames != NULL)
+        free(ppExtensionNames);
+
+    if(result != VK_SUCCESS) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "vkCreateInstance returned %i", result);
+        return -4;
+    }
+
+    return 1;
+}
+
+int initVulkan() {
+    context.instance = NULL;
+
+    int returnCode;
+
+    returnCode = initInstance();
+    if( returnCode < 0 )
+        return returnCode;
+
+    //Uint32 physicalDevicesCount = 0;
+
+    //vkEnumeratePhysicalDevices(context.instance, &physicalDevicesCount, NULL);
+
+    return 1;
+}
+
+int main(int argc, char **argv) {
+    int returnCode;
+
+    if(SDL_Init( SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0 ) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init failed with %s", SDL_GetError());
+        return -1;
+    }
+
+    context.pWindow = SDL_CreateWindow(context.title, context.x, context.y, context.w, context.h, context.flags);
+
+    if(context.pWindow == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_CreateWindow failed with %s", SDL_GetError());
+        return -2;
+    }
+
+    returnCode = initVulkan();
+
+
+    if( returnCode >= 0 )
+        loop();
+
+    vkDestroyInstance(context.instance, NULL);
+    SDL_DestroyWindow(context.pWindow);
+
+    return returnCode;
 }
