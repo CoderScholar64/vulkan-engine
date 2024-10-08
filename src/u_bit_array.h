e605dfd1af8ea84a3eb9f09d74948b598dfedbf3
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-10-08 13:45:45 -0700

    u_bit_array is more clearly named

--- src/u_bit_array.h
+++ src/u_bit_array.h
@@ -7,13 +7,11 @@ typedef unsigned u_bit_element;
 
 #define U_BIT_ELEMENT_BIT_COUNT (sizeof(u_bit_element) * U_BITS_PER_BYTE)
 
-#define U_BIT_ARRAY_INDEX_AMOUNT(arraySize) (arraySize / U_BIT_ELEMENT_BIT_COUNT + (0 != (arraySize % U_BIT_ELEMENT_BIT_COUNT)))
+#define U_BIT_ARRAY_INDEX_AMOUNT(arrayElementAmount) (arrayElementAmount / U_BIT_ELEMENT_BIT_COUNT + (0 != (arrayElementAmount % U_BIT_ELEMENT_BIT_COUNT)))
 
-#define U_BIT_ARRAY_SIZE(arraySize) (sizeof(u_bit_element) * U_BIT_ARRAY_INDEX_AMOUNT(arraySize))
+#define U_BIT_ARRAY_SIZE(arrayElementAmount) (sizeof(u_bit_element) * U_BIT_ARRAY_INDEX_AMOUNT(arrayElementAmount))
 
-#define U_BIT_ARRAY_GET(array, index) (\
-    (array[index / U_BIT_ELEMENT_BIT_COUNT] >> (index % U_BIT_ELEMENT_BIT_COUNT)) & 1\
-)
+#define U_BIT_ARRAY_GET(array, index) ((array[index / U_BIT_ELEMENT_BIT_COUNT] >> (index % U_BIT_ELEMENT_BIT_COUNT)) & 1)
 
 #define U_BIT_ARRAY_SET(array, index, bitValue)\
     array[index / U_BIT_ELEMENT_BIT_COUNT] &= (~((u_bit_element)0)) ^ (1 << (index % U_BIT_ELEMENT_BIT_COUNT));\
