803a0f2403385804097f43b0d5c4955c979aa8ae
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-10-08 00:10:32 -0700

    Applied a 4x5 grid with seed 555.
    This is a testing maze. I found this number through the brute force of
    c and my computer.
    
    The only reason why no 4x4 grid is that it took too long for my computer
    to find this value.

--- src/v_init.c
+++ src/v_init.c
@@ -156,8 +156,8 @@ VEngineResult v_init(Context *this) {
         printf("Model name = %s. Length = %zu decodedIndex = %i\n", this->vk.pModels[i].name, lengthOfName, decodedIndex);
     }
 
-    UMazeData mazeData = u_maze_gen_data(13, 11);
-    UMazeGenResult mazeGenResult = u_maze_gen(&mazeData, 29, 1);
+    UMazeData mazeData = u_maze_gen_data(4, 5);
+    UMazeGenResult mazeGenResult = u_maze_gen(&mazeData, 555, 1);
 
     size_t mazePieceAmounts[16] = { 0 };
 
