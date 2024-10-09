6beb140dffd6c4a43ba21bca8f87cc9603681853
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-10-08 21:30:22 -0700

    This function should be changed.

--- src/u_maze.h
+++ src/u_maze.h
@@ -3,7 +3,13 @@
 
 #include "u_maze_def.h"
 
-UMazeData u_maze_gen_full_sq_grid(unsigned width, unsigned height);
+/**
+ * This generates a grid of data.
+ * @param width The amount of vertices would the grid have in the width axis.
+ * @param depth The amount of vertices would the grid have in the depth axis.
+ * @return UMazeData struct. If data members inside the struct are NULL then this allocation had failed.
+ */
+UMazeData u_maze_gen_full_sq_grid(unsigned width, unsigned depth);
 
 void u_maze_delete_data(UMazeData *pMazeData);
 
