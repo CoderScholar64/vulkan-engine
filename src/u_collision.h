ae12f50de6fb638bf56a2c8d17e0083615487905
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-10-15 22:40:47 -0700

    Update documentation

--- src/u_collision.h
+++ src/u_collision.h
@@ -8,7 +8,7 @@
  * @param pPoly0 The first polyhedron to compute. @warning Do not put a zero point polyhedron.
  * @param pPoly1 The second polyhedron to compute. @warning Do not put a zero point polyhedron.
  * @param pBackoutCache The back out cache if it is needed. @note This can actually be NULL, but then the backout code will not be run.
- * @return U_GJK_NO_COLLISION if there is no collision. U_GJK_COLLISION if the collision happended U_GJK_NOT_DETERMINED if the collision cannot be confirmed.
+ * @return U_COLLISION_FALSE if there is no collision. U_COLLISION_TRUE if the collision happened U_COLLISION_NOT_DETERMINED if the collision cannot be confirmed.
  *          UCollisionReturn::normal and UCollisionReturn::depth is returned if pBackoutCache is used for this function. @note To backout pPoly0 multiply the normal by negative depth and add it to pPoly0
  */
 UCollisionReturn u_collision_poly(const UCollisionPolyhedron *pPoly0, const UCollisionPolyhedron *pPoly1, UCollisionBackoutCache *pBackoutCache);
@@ -18,7 +18,7 @@ UCollisionReturn u_collision_poly(const UCollisionPolyhedron *pPoly0, const UCol
  * @param pPoly Polyhedron to compute. @warning Do not put a zero point polyhedron.
  * @param pSphere The sphere to test the collision with.
  * @param pBackoutCache The back out cache if it is needed. @note This can actually be NULL, but then the backout code will not be run.
- * @return U_GJK_NO_COLLISION if there is no collision. U_GJK_COLLISION if the collision happended U_GJK_NOT_DETERMINED if the collision cannot be confirmed.
+ * @return U_COLLISION_FALSE if there is no collision. U_COLLISION_TRUE if the collision happened U_COLLISION_NOT_DETERMINED if the collision cannot be confirmed.
  *          UCollisionReturn::normal and UCollisionReturn::depth is returned if pBackoutCache is used for this function. @note To backout pPoly multiply the normal by negative depth and add it to pPoly
  */
 UCollisionReturn u_collision_poly_sphere(const UCollisionPolyhedron *pPoly, const UCollisionSphere *pSphere, UCollisionBackoutCache *pBackoutCache);
@@ -28,7 +28,7 @@ UCollisionReturn u_collision_poly_sphere(const UCollisionPolyhedron *pPoly, cons
  * @note This function is faster than every single test.
  * @param pSphere0 A sphere to test the collision with.
  * @param pSphere1 A sphere to test the collision with.
- * @return U_GJK_NO_COLLISION if there is no collision. U_GJK_COLLISION if the collision happended U_GJK_NOT_DETERMINED if the collision cannot be confirmed.
+ * @return U_COLLISION_FALSE if there is no collision. U_COLLISION_TRUE if the collision happened U_COLLISION_NOT_DETERMINED if the collision cannot be confirmed.
  *          UCollisionReturn::normal and UCollisionReturn::depth is also returned. @note To backout pPoly multiply the normal by negative depth and add it to pPoly
  */
 UCollisionReturn u_collision_sphere(const UCollisionSphere *pSphere0, const UCollisionSphere *pSphere1);
@@ -36,7 +36,7 @@ UCollisionReturn u_collision_sphere(const UCollisionSphere *pSphere0, const UCol
 /**
  * This function allocates the backout cache.
  * @note This uses math to make a backout cache hold a polyhedron. Triangles, edges and vertices increases as extraVertices
- * @warning Use u_gjk_free_backout_cache to delete the returned backout cache.
+ * @warning Use u_collision_free_backout_cache to delete the returned backout cache.
  * @param extraVertices The amount of extra vertices to add. @note The amount of zero will still work, but other functions will not work well with the returned result.
  * @return A backout cache used to calculate getting shapes out of shapes.
  */
