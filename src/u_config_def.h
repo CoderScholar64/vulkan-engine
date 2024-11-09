d32a0097a1f177fd90fa8e85dd7f9d46cbd472ed
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-11-09 10:34:19 -0800

    Configuration struct

---
+++ src/u_config_def.h
@@ -0,0 +1,12 @@
+#ifndef U_CONFIG_DEF_29
+#define U_CONFIG_DEF_29
+
+typedef struct UConfiguration {
+    int width;
+    int height;
+    int sample_count;
+    int graphics_card_index;
+    int pixel_format_index;
+} UConfiguration;
+
+#endif // U_CONFIG_DEF_29
--- src/u_vector_def.h
+++ src/u_vector_def.h
@@ -1,8 +1,6 @@
 #ifndef U_VECTOR_DEF_29
 #define U_VECTOR_DEF_29
 
-#include <stddef.h>
-
 typedef struct UVector {
     size_t elementSize;
     size_t size;
