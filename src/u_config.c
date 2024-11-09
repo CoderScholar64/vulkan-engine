cc5de9d2ffc6caed0624184d8cce8f283b890f65
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-11-09 10:47:40 -0800

    Configuration is being implemented

---
+++ src/u_config.c
@@ -0,0 +1,13 @@
+#include "u_config.h"
+
+int u_config_defaults(UConfiguration *pConfig) {
+    //
+}
+
+int u_config_load(UConfiguration *pConfig) {
+    //
+}
+
+int u_config_save(const UConfiguration *const pConfig) {
+    //
+}
---
+++ src/u_config.h
@@ -0,0 +1,12 @@
+#ifndef U_CONFIG_29
+#define U_CONFIG_29
+
+#include "u_config_def.h"
+
+int u_config_defaults(UConfiguration *pConfig);
+
+int u_config_load(UConfiguration *pConfig);
+
+int u_config_save(const UConfiguration *const pConfig);
+
+#endif // U_CONFIG_29
